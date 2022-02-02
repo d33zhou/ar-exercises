76f5e6423728ec9610e92684fc9b726cfd3bce6e
d33zhou <d33zhou@outlook.com>
2022-02-02 06:35:15 +0000

    complete exercise 5

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -7,4 +7,11 @@ require_relative './exercise_4'
 puts "Exercise 5"
 puts "----------"
 
-# Your code goes here ...
+puts "Total Annual Revenue:"
+puts Store.sum("annual_revenue")
+
+puts "Average Annual Revenue:"
+puts Store.average("annual_revenue")
+
+puts "Store with annual sales greater than $1M:"
+puts Store.where("annual_revenue >= ?", 1000000).count
